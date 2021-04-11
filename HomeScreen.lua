6c3e4b005ec8ba2e5f40e17ea14406533b2cdc27
Victor Åkesson <Victorakesson@gmail.com>
2021-04-11 20:41:23 +0200

    Made my code prettier

--- HomeScreen.lua
+++ HomeScreen.lua
@@ -2,8 +2,9 @@ gameOver = false
 pauseGame = false
 Menu = true
 tutorial = false
+victory = false
 
 fontcolor = {3, 1, 3, 1}
-bigFont = love.graphics.newFont("OpenSans-Bold.ttf", 100)
-smallFont = love.graphics.newFont("OpenSans-Bold.ttf", 25)
-miniFont = love.graphics.newFont("OpenSans-Bold.ttf", 15)
+bigFont = love.graphics.newFont("Assets/OpenSans-Bold.ttf", 100)
+smallFont = love.graphics.newFont("Assets/OpenSans-Bold.ttf", 25)
+miniFont = love.graphics.newFont("Assets/OpenSans-Bold.ttf", 15)
--- main.lua
+++ main.lua
@@ -1,15 +1,15 @@
 love.physics.setMeter(64) 
 
-local x = 50 
-local y = 750
-local font 
+x = 50 
+y = 750
 
-local world = love.physics.newWorld(0, 9.82*64, true) -- This give the world gravity 
+
+world = love.physics.newWorld(0, 9.82*64, true) -- This give the world gravity 
 -- We need to set setMeter to 64 so we can make the world accept 64 bits as a meter which we then put in the Local world code
 
-local seconds = 0 -- Here we put seconds to 0 so we use 0 as the start time
+seconds = 0 -- Here we put seconds to 0 so we use 0 as the start time
 
-local charachter = {} --This is the charachter.
+charachter = {} --This is the charachter.
 
 charachter.body = love.physics.newBody(world, x, y, 'dynamic')
 charachter.body.setMass(charachter.body, 25)
@@ -17,105 +17,50 @@ charachter.shape = love.physics.newRectangleShape(0, 0, 50, 50)
 charachter.fixture = love.physics.newFixture(charachter.body, charachter.shape)
 
 
-local bar = {} -- These are the plattforms they are almost identical except for shape and size.
+bar = {} -- These are the plattforms they are almost identical except for shape and size.
 -- förutom platsen dem är på. 
 bar.body = love.physics.newBody(world, -50, 775, 'static') -- The plattform.
 bar.shape = love.physics.newPolygonShape(0, 0, 0, 20, 200, 20, 200, 0) -- The plattforms measurements.
 bar.fixture = love.physics.newFixture(bar.body, bar.shape) 
 
-local barsecond = {}
+barsecond = {}
 barsecond.body = love.physics.newBody(world, 700, 775, 'static') -- 600 is where the plattform is placed horizontaly.
 barsecond.shape = love.physics.newPolygonShape(0, 0, 0, 20, 200, 20, 200, 0)
 barsecond.fixture = love.physics.newFixture(barsecond.body, barsecond.shape)
 
-local barthree = {}
+barthree = {}
 barthree.body = love.physics.newBody(world, 300, 550, 'static')
 barthree.shape = love.physics.newPolygonShape(0, 0, 0, 20, 200, 20, 200, 0)
 barthree.fixture = love.physics.newFixture(barthree.body, barthree.shape)
 
 
 
-local barfour = {}
+barfour = {}
 barfour.body = love.physics.newBody(world, 600, 350, 'static') 
 barfour.shape = love.physics.newPolygonShape(0, 0, 0, 20, 200, 20, 200, 0)
 barfour.fixture = love.physics.newFixture(barfour.body, barfour.shape)
 
-local barfive = {}
+barfive = {}
 barfive.body = love.physics.newBody(world, 600, 150, 'static') 
 barfive.shape = love.physics.newPolygonShape(0, 0, 0, 20, 200, 20, 200, 0)
 barfive.fixture = love.physics.newFixture(barfive.body, barfive.shape)
 
-local flag 
+flag = {} 
 function love.load()
-flag = love.graphics.newImage("flag.png")
+flag = love.graphics.newImage("Assets/flag.png")
 end
-
-
-
-
 
 love.graphics.setBackgroundColor(0.2, 180, 0) -- Changes the backgroundcolor
   love.window.setMode(800, 800 ) --This is how big the widow that love opens is in my case its 800x800 because my last plattform starts at 600.
 
-  love.draw = function() -- Draws the whole code.
-  
-
-  if victory == true then 
-    love.graphics.print({fontcolor, ("VICTORY!!!")}, bigFont, 200, 150)
-    love.graphics.print({fontcolor, ("R To Restart")}, smallFont, 256, 400)
-    return 
-  elseif gameOver == true then
-      love.graphics.print({fontcolor, ("Game")}, bigFont, 200, 150)
-      love.graphics.print({fontcolor, ("Over")}, bigFont , 220, 250)
-      love.graphics.print({fontcolor, ("R To Restart")}, smallFont, 256, 400)
-      love.graphics.print({fontcolor, ("Esc To Exit")}, smallFont, 266, 450)
-      return
-    elseif pauseGame == true then
-      love.graphics.print({fontcolor, ("ClIMB RACE")}, bigFont , 110, 150) 
-      love.graphics.print({fontcolor, ("Paused")}, smallFont , 210, 300) 
-      love.graphics.print({fontcolor, ("Press P To Continue")}, smallFont , 210, 350) 
-      return
-    elseif Menu == true and tutorial == false then 
-      love.graphics.print({fontcolor, ("Welcome To")}, smallFont , 250, 150) 
-      love.graphics.print({fontcolor, ("CLIMB RACE")}, bigFont , 110, 200) 
-      love.graphics.print({fontcolor, ("Press ENTER To Start The Game")}, smallFont , 170, 400)
-      return
-      
-    end
-    if tutorial == true and pauseGame == false then
-      love.graphics.print({fontcolor, ("CLIMB RACE")}, bigFont , 110, 130) 
-      love.graphics.print({fontcolor, ("T To Continue")}, smallFont , 145, 300) 
-      love.graphics.rectangle('line', 139, 305, 30, 30)
-      love.graphics.print({fontcolor, ("Esc To Exit")}, smallFont , 345, 300)
-      love.graphics.rectangle('line', 339, 305, 54, 30)
-      love.graphics.print({fontcolor, ("R To Restart")}, smallFont , 145, 360) 
-      love.graphics.rectangle('line', 139, 365, 30, 30)
-      love.graphics.print({fontcolor, ("P To Pause")}, smallFont , 345, 360) 
-      love.graphics.rectangle('line', 339, 365, 30, 30)
-      love.graphics.print({fontcolor, ("Arrows To Move Sideways")}, smallFont , 170, 479) 
-      return
-    end
-    love.graphics.draw(flag, 675, 100,0, 0.2)
-    love.graphics.setColor(0.3, 0.3, 0)
-    love.graphics.polygon('fill', barsecond.body:getWorldPoints(bar.shape:getPoints()))
-    love.graphics.polygon('fill', bar.body:getWorldPoints(bar.shape:getPoints()))
-    love.graphics.polygon('fill', barthree.body:getWorldPoints(bar.shape:getPoints()))
-    love.graphics.polygon('fill', barfour.body:getWorldPoints(bar.shape:getPoints()))
-    love.graphics.polygon('fill', barfive.body:getWorldPoints(bar.shape:getPoints()))
-    love.graphics.setColor(0.7, 0, 0,2)
-    love.graphics.polygon('fill', charachter.body:getWorldPoints(charachter.shape:getPoints()))
-    local clock_display = 'Time: ' .. seconds --Makes the timer show seconds.
-    love.graphics.print(clock_display, 0, 0, 0, 1.1, 1.1) -- The clocks place and color.
-  
-  end
 
 love.update = function(dt) -- This is the function for time and controls.
   world:update(dt) -- Updates the world time(dt).
 
   seconds = seconds + dt -- This makes seconds which we gave the base number 0 to increase with dt (The time the world has been up).
 
-  local charachterY = charachter.body:getY()
-  local charachterX = charachter.body:getX()
+  charachterY = charachter.body:getY()
+  
 
   if(charachterY > love.graphics.getHeight()) then 
     gameOver = true 
@@ -145,7 +90,7 @@ end
 
 HomeScreen = require("HomeScreen")
 keys = require("keys")
-
+require "lovedraw"
 
 
 
